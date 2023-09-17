6f737272d63fafae9bf3ac78696a3208efa4311f
Steven John <stevenjpx2@gmail.com>
2023-09-17 21:17:05 +0530

    feat: first commit

---
+++ src/module.ts
@@ -0,0 +1,21 @@
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+
+import {} from "../package.json";
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: "my-module",
+    configKey: "myModule",
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve("./runtime/plugin"));
+  },
+});
